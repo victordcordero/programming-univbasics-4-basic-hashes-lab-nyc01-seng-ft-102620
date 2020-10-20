53025e8c705e37c5fe027593a43b9533ce8e6e00
Victor Cordero <victordcordero@gmail.com>
2020-10-20 19:53:32 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -0,0 +1,18 @@
+def new_hash
+{}
+end
+
+def my_hash
+  {:apple => "food",
+  : dog => "pet"
+  }
+end
+
+def pioneer
+  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+end
+
+
+def id_hash_generator(number)
+  # return a hash with a key :id assigned to the provided number
+end
